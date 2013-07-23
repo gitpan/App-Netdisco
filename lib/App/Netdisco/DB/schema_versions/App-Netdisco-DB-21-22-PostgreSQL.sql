a7d5766cdb76fd121fea6075a137394cc2be5b25
Oliver Gorwits <oliver@cpan.org>
2013-07-23 23:02:00 +0000

    Import of OLIVER/App-Netdisco-2.010002 from CPAN.
    gitpan-cpan-distribution: App-Netdisco
    gitpan-cpan-version:      2.010002
    gitpan-cpan-path:         OLIVER/App-Netdisco-2.010002.tar.gz
    gitpan-cpan-author:       OLIVER
    gitpan-cpan-maturity:     released

--- lib/App/Netdisco/DB/schema_versions/App-Netdisco-DB-21-22-PostgreSQL.sql
+++ lib/App/Netdisco/DB/schema_versions/App-Netdisco-DB-21-22-PostgreSQL.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-ALTER TABLE device_port ALTER COLUMN remote_id TYPE bytea USING remote_id::bytea;
+-- ALTER TABLE device_port ALTER COLUMN remote_id TYPE bytea USING remote_id::bytea;
 
 COMMIT;
